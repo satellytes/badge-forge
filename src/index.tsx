98ac61605543a36fb2aea3860b69b4fd12f38ab0
timon-so <timon.sommer@satellytes.com>
2022-08-24 10:59:12 +0200

    feat: use Panelbear NPM package for analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as Panelbear from "@panelbear/panelbear-js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,3 +17,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+Panelbear.load("3TV3m1B7O7p");
+Panelbear.trackPageview();
